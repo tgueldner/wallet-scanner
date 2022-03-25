496cb4c6e2adee98af7c0e482a625f91dae05691
TG <t.gueldner@gmx.de>
2022-03-25 12:12:29 +0100

    inntroduce parallel work

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import logging
 import logging.config
 import os
+
+import ScanWatch
 import yaml
+import asyncio
+import concurrent.futures
+from threading import Thread
 from keys.api import ETH_API_KEY, BSC_API_KEY, POLY_API_KEY
 from keys.telegram import TELEGRAM_CHAT_ID, TELEGRAM_TOKEN
 from web3 import Web3
-import time
+from asyncio_throttle import Throttler
+
+q = asyncio.Queue(10)
+throttler = Throttler(rate_limit=4, period=1)  # throttle to 5/sec
 
 w3 = Web3()
 
@@ -34,35 +42,53 @@ def setup_logging():
         logging.config.dictConfig(config)
 
 
-def generateAccount():
-    return w3.eth.account.create()
+async def generateAccount():
+    while True:
+        account = w3.eth.account.create()
+        await q.put(account)
+        logger.debug('Produced {}'.format(account.address))
+        await asyncio.sleep(0.1)
 
 
-def handleHit(account, balances):
-    logger.info("HIT for {}".format(account.address))
+def handleHit(account, client):
+    logger.info("HIT on {] for {}".format(client, account.address))
     logger.info("private key: {}".format(account.key.hex()))
-    return
 
 
-def checkBalance(address):
-    result = {}
-    for client in clients:
-        balance = client.get_balance(address)
-        logger.debug(("{}: {} = {}").format(client.nt_type, address, balance))
-        result[client.nt_type] = balance
-    return result
+def checkAccount(account, client):
+    try:
+        balance = client.get_balance(account.address)
+        logger.debug(("{}: {} = {}").format(client.nt_type, account.address, balance))
+        if balance > 0:
+            handleHit(account, client)
+    except ScanWatch.exceptions.APIException as err:
+        logger.debug("Error on client {}: {}".format(client.nt_type, err))
+
+
+async def consumer():
+    while True:
+        account = await q.get()
+        async with throttler:
+            for client in clients:
+                thread = Thread(target=checkAccount, args=[account, client])
+                thread.start()
+        await asyncio.sleep(0.1)
 
 
 if __name__ == "__main__":
     setup_logging()
     logger = logging.getLogger(__name__)
+    logger.info("Scanner started!")
+    loop = asyncio.get_event_loop()
+    for i in range(5):
+        loop.create_task(generateAccount())
 
-    while(True):
-        account = generateAccount()
-        balances = checkBalance(account.address)
-        balance = sum(balances.values())
-        if balance > 0:
-            handleHit(account, balances)
+    for i in range(5):
+        loop.create_task(consumer())
+
+    loop.run_forever()
+
+
 
 #manager = ScanManager(ethAddress, NETWORK.ETHER, ETH_API_KEY)
 #manager = ScanManager(bscAddress, NETWORK.BSC, BSC_API_KEY)
