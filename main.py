dfbef68e203480bfbef5eabf390b3cffbe402668
tgueldner <t.gueldner@gmx.de>
2022-03-28 01:41:40 +0200

    remove asyncio, switch to threading

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import logging
 import logging.config
 import os
+import threading
+import time
+from queue import Queue
 
 import ScanWatch
 import yaml
-import asyncio
-import concurrent.futures
 from threading import Thread
 from keys.api import ETH_API_KEY, BSC_API_KEY, POLY_API_KEY
 from keys.telegram import TELEGRAM_CHAT_ID, TELEGRAM_TOKEN
 from web3 import Web3
-from asyncio_throttle import Throttler
-
-q = asyncio.Queue(10)
-throttler = Throttler(rate_limit=4, period=1)  # throttle to 5/sec
 
 w3 = Web3()
 
@@ -21,18 +18,18 @@ from ScanWatch.ScanManager import ScanManager
 from ScanWatch.Client import Client
 from ScanWatch.utils.enums import NETWORK, TRANSACTION
 
-#ethAddress = "0x29c8F2A238F0e2c90cf72133873EbcAA31bD1C4c"
-#bscAddress = "0xc5b793CFDc0a62911d6Ce1D209200062529eF8A0"
+clients = {
+    NETWORK.ETHER: Client(ETH_API_KEY, NETWORK.ETHER),
+    NETWORK.BSC: Client(BSC_API_KEY, NETWORK.BSC),
+    NETWORK.POLYGON: Client(POLY_API_KEY, NETWORK.POLYGON)
+}
 
-clients = [
-    Client(ETH_API_KEY, NETWORK.ETHER),
-    Client(BSC_API_KEY, NETWORK.BSC),
-    Client(POLY_API_KEY, NETWORK.POLYGON)
-    ]
+queues = {k: Queue(10) for k in clients.keys()}
 
 logging_yaml_path = os.path.abspath(
     os.path.join(os.path.dirname(__file__), "resources", "logging_config.yaml")
 )
+
 
 def setup_logging():
     with open(logging_yaml_path, "r") as f:
@@ -42,16 +39,22 @@ def setup_logging():
         logging.config.dictConfig(config)
 
 
-async def generateAccount():
+def generateAccount():
     while True:
         account = w3.eth.account.create()
-        await q.put(account)
-        logger.debug('Produced {}'.format(account.address))
-        await asyncio.sleep(0.1)
+        for c in clients.values():
+            queues[c.nt_type].put(account)
+        logger.debug('Produced {} (fill level {})'
+            .format(
+                account.address,
+                '/'.join([str(q.qsize()) for q in queues.values()])
+            )
+        )
+        time.sleep(0.1)
 
 
 def handleHit(account, client):
-    logger.info("HIT on {] for {}".format(client, account.address))
+    logger.info("HIT on {] for {}".format(client.nt_type, account.address))
     logger.info("private key: {}".format(account.key.hex()))
 
 
@@ -61,45 +64,45 @@ def checkAccount(account, client):
         logger.debug(("{}: {} = {}").format(client.nt_type, account.address, balance))
         if balance > 0:
             handleHit(account, client)
-    except ScanWatch.exceptions.APIException as err:
+        time.sleep(1)
+    except Exception as err:
         logger.debug("Error on client {}: {}".format(client.nt_type, err))
+        print(err)
 
 
-async def consumer():
+def worker(network, number):
+    logger.debug("Start worker {}#{}".format(network, number))
     while True:
-        account = await q.get()
-        async with throttler:
-            for client in clients:
-                thread = Thread(target=checkAccount, args=[account, client])
-                thread.start()
-        await asyncio.sleep(0.1)
+        q = queues[network]
+        account = q.get()
+        checkAccount(account, clients[network])
+        q.task_done()
 
 
 if __name__ == "__main__":
     setup_logging()
     logger = logging.getLogger(__name__)
+
+    for network in clients.keys():
+        for i in range(5):
+            t = Thread(target=worker, args=(network, i,))
+            t.start()
+    Thread(target=generateAccount).start()
+
     logger.info("Scanner started!")
-    loop = asyncio.get_event_loop()
-    for i in range(5):
-        loop.create_task(generateAccount())
+    while True:
+        time.sleep(1)
 
-    for i in range(5):
-        loop.create_task(consumer())
-
-    loop.run_forever()
-
-
-
-#manager = ScanManager(ethAddress, NETWORK.ETHER, ETH_API_KEY)
-#manager = ScanManager(bscAddress, NETWORK.BSC, BSC_API_KEY)
+# manager = ScanManager(ethAddress, NETWORK.ETHER, ETH_API_KEY)
+# manager = ScanManager(bscAddress, NETWORK.BSC, BSC_API_KEY)
 # 4 Transactions Update
-#manager.update_all_transactions()
+# manager.update_all_transactions()
 # 5 Transactions
-#manager.get_transactions(TRANSACTION.NORMAL)
-#manager.get_transactions(TRANSACTION.INTERNAL)
+# manager.get_transactions(TRANSACTION.NORMAL)
+# manager.get_transactions(TRANSACTION.INTERNAL)
 # 6 Holdings
-#erc20 = manager.get_erc20_holdings()
-#print(erc20)
+# erc20 = manager.get_erc20_holdings()
+# print(erc20)
 
-#erc721 = manager.get_erc721_holdings()
-#print(erc721)
+# erc721 = manager.get_erc721_holdings()
+# print(erc721)
